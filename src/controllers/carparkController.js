f72ec5e2e35ace770d9b5dcac16d4c8da5da7eec
LCA-95 <luicaian@hotmail.com>
2024-07-15 02:57:21 +0800

    Add in try catch

--- src/controllers/carparkController.js
+++ src/controllers/carparkController.js
@@ -1,33 +1,32 @@
 const carparkRepository = require("../repository/carparkRepository");
 const config = require("../config/config");
 const getCarParks = async (req, res) => {
-  let {
-    isFreeParking,
-    isNightParkingAllowed,
-    gantryHeight,
-    page = config.PAGE_NUMBER,
-    limit = config.PAGE_SIZE,
-  } = req.query;
-
-  const filterCriteria = {};
-  isFreeParking = isFreeParking ? JSON.parse(isFreeParking) : null;
-
-  // If want to check isFreeParking Allowed, just check if is column contains value as carpark without freeparking will not store anything even the data source is showing "NO"
-  if (isFreeParking) {
-    filterCriteria.freeParking = { not: null };
-  } else if (isFreeParking === false) {
-    filterCriteria.freeParking = null;
-  }
-
-  if (isNightParkingAllowed) {
-    filterCriteria.isNightParkingAllowed = JSON.parse(isNightParkingAllowed);
-  }
-
-  if (gantryHeight) {
-    filterCriteria.gantryHeight = { gte: parseFloat(gantryHeight) };
-  }
-
   try {
+    let {
+      isFreeParking,
+      isNightParkingAllowed,
+      gantryHeight,
+      page = config.PAGE_NUMBER,
+      limit = config.PAGE_SIZE,
+    } = req.query;
+
+    const filterCriteria = {};
+    isFreeParking = isFreeParking ? JSON.parse(isFreeParking) : null;
+
+    // If want to check isFreeParking Allowed, just check if is column contains value as carpark without freeparking will not store anything even the data source is showing "NO"
+    if (isFreeParking) {
+      filterCriteria.freeParking = { not: null };
+    } else if (isFreeParking === false) {
+      filterCriteria.freeParking = null;
+    }
+
+    if (isNightParkingAllowed) {
+      filterCriteria.isNightParkingAllowed = JSON.parse(isNightParkingAllowed);
+    }
+
+    if (gantryHeight) {
+      filterCriteria.gantryHeight = { gte: parseFloat(gantryHeight) };
+    }
     const filteredCarparks = await carparkRepository.getCarparks(
       filterCriteria,
       parseInt(page),
@@ -39,12 +38,12 @@ const getCarParks = async (req, res) => {
       data: filteredCarparks.data,
       pagination: filteredCarparks.pagination,
     });
-  } catch (error) {
-    console.error("Error: ", error);
+  } catch (err) {
+    console.error("Error: ", err);
     res.status(500).json({
       status: "error",
       message: "An error occurred while retrieving car parks",
-      error: error.message,
+      error: err.message,
     });
   }
 };
